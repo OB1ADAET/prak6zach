59206d54ba8e27aac6881a0e872df043204a08da
OB1ADAET <116683120+OB1ADAET@users.noreply.github.com>
2023-02-09 16:16:43 +0300

    Add files via upload

---
+++ BAby.cs
@@ -0,0 +1,9 @@
+﻿namespace prak6
+{
+    public class BAby
+    {
+        public string author = string.Empty;
+        public string[] date = new string[Program.init];
+        public string[] content = new string[Program.init];
+    }
+}
